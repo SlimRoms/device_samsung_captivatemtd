54542e82d89123729ca25e68d7823bb2bec40861
prbassplayer <prbassplayer@gmail.com>
2013-02-08 06:52:49 +0100

    General cleanup for i897
    Change-Id: Ib936d6c1312375b423f300aefeabb315ddf1ea86

--- slim.mk
+++ slim.mk
@@ -16,13 +16,7 @@ $(call inherit-product, vendor/slim/config/common_sgs.mk)
 #copy kernel and modules
 PRODUCT_COPY_FILES += \
         device/samsung/aries-common/recovery.fstab:ramdisk.img \
-        device/samsung/aries-common/recovery.fstab:recovery/root/etc/recovery.fstab \
-#	device/samsung/captivatemtd/prebuilt/kernel/boot.img:system/slimkernel/boot.img \
-#	device/samsung/captivatemtd/prebuilt/kernel/boot.img:kernel \
-#	device/samsung/captivatemtd/prebuilt/kernel/system/lib/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
-#	device/samsung/captivatemtd/prebuilt/kernel/system/lib/modules/cifs.ko:system/lib/modules/cifs.ko \
-#	device/samsung/captivatemtd/prebuilt/kernel/system/lib/modules/mls_utf8.ko:system/lib/modules/mls_utf8.ko \
-#	device/samsung/captivatemtd/prebuilt/kernel/system/lib/modules/nls_utf8.ko:system/lib/modules/nls_utf8.ko
+        device/samsung/aries-common/recovery.fstab:recovery/root/etc/recovery.fstab
 
 
 PRODUCT_COPY_FILES +=  \
@@ -33,7 +27,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 #copy 00check
 PRODUCT_COPY_FILES += \
-	device/samsung/captivatemtd/prebuilt/kernel/system/etc/init.d/00check:system/etc/init.d/00check
+    device/samsung/captivatemtd/prebuilt/init.d/00check:system/etc/init.d/00check
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := captivatemtd
