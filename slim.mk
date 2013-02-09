1a9d71f66312c2109ad71b32a27c69a7c6e47bed
prbassplayer <prbassplayer@gmail.com>
2013-02-09 02:35:56 +0100

    Cleanup #2 for i897
    Change-Id: I43164f82578bdc9366e7d3f3612fba43ddb3beb4

--- slim.mk
+++ slim.mk
@@ -13,12 +13,6 @@ $(call inherit-product, device/samsung/captivatemtd/full_captivatemtd.mk)
 # Inherit device settings
 $(call inherit-product, vendor/slim/config/common_sgs.mk)
 
-#copy kernel and modules
-PRODUCT_COPY_FILES += \
-        device/samsung/aries-common/recovery.fstab:ramdisk.img \
-        device/samsung/aries-common/recovery.fstab:recovery/root/etc/recovery.fstab
-
-
 PRODUCT_COPY_FILES +=  \
     vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
@@ -36,5 +30,5 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-I897
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I897 TARGET_DEVICE=SGH-I897 BUILD_FINGERPRINT=samsung/SGH-I897/SGH-I897:4.2.1/JOP40D:user/release-keys PRIVATE_BUILD_DESC="SGH-I897-userdebug 4.2.1 JOP40D user.Josh.20120828.002056 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I897 TARGET_DEVICE=SGH-I897 BUILD_FINGERPRINT=samsung/SGH-I897/SGH-I897:2.3.5/GINGERBREAD/UCKK4:user/release-keys PRIVATE_BUILD_DESC="SGH-I897-user 2.3.5 GINGERBREAD UCKK4 release-keys"
 
